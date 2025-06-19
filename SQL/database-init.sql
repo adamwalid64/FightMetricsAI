e8cee37815348a0521eea08b0a6f6f5a05b84105
Adam Walid <adamwalidschool@gmail.com>
2025-06-19 15:32:50 -0400

    added new datasets

--- SQL/database-init.sql
+++ SQL/database-init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS ufc_data;
 USE ufc_data;
 
-
+-- Initialize Starter DB
 CREATE TABLE ufc_fighters (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30),
@@ -25,10 +25,28 @@ CREATE TABLE ufc_fighters (
     TD_Acc INT NOT NULL,
     TD_Def INT NOT NULL,
     Sub_Avg DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id)	
 );
 
-SELECT * FROM your_table_name
-LIMIT 2000;
+SELECT * FROM ufc_data.ufc_fighters LIMIT 5000;
 
 SELECT user FROM mysql.user;
+
+-- DATA CLEANING
+
+USE ufc_data;
+
+CREATE VIEW clean_ufc_fights AS
+SELECT *
+FROM ufc_data.ufc_fighters
+WHERE 
+    age IS NOT NULL
+    AND SLpM != 0.00
+    AND Str_Acc != 0
+    AND SApM != 0.00
+    AND Str_Def != 0
+    AND TD_Avg != 0.00
+    AND TD_Acc != 0
+    AND TD_Def != 0
+    AND Sub_Avg != 0.00;
+
