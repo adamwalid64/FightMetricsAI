fe70b176c09cccdb5443ced795f32a37d12b676d
Adam Walid <adamwalidschool@gmail.com>
2025-06-25 19:19:26 -0400

    backend changes

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -7,12 +7,21 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), '..', 'Prediction', 'xgb_ufc_model.pkl')
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'xgb_ufc_model.pkl')
 model = joblib.load(MODEL_PATH)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
+
+    features = data.get('features')
+    fighter_one = data.get('fighterOne')
+    fighter_two = data.get('fighterTwo')
+
+    print('Fighter One:', fighter_one)
+    print('Fighter Two:', fighter_two)
+    print('Features:', features)
+
     features = np.array(data.get('features')).reshape(1, -1)
     pred = model.predict(features)[0]
     return jsonify({'prediction': pred})
